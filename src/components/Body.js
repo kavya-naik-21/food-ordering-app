465c0281fde9cc27119c00cc39f6661a2439bebd
Kavya Naik <kavyanaik2112@gmail.com>
2025-02-06 22:18:54 +0530

    using hooks-top rated restaurants

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,26 @@
 import RestaurantCard from "./RestaurantCard";
 import { restaurants } from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
+  const [lisOfRestaurants, setlisOfRestaurants] = useState(restaurants);
   return (
     <div className="body">
-      <div className="search-container">Search</div>
+      <div className="filter-restaurants">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const updatedlisOfRestaurants = lisOfRestaurants.filter((restaurant) => {
+              return restaurant.info.avgRating > 4.3;
+            });
+            setlisOfRestaurants(updatedlisOfRestaurants)
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
       <div className="restaurants-container">
-        {restaurants.map((restaurant) => (
+        {lisOfRestaurants.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
