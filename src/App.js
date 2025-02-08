45d2ffad0575dd0c80ff8a3355a67744d36c10a1
Kavya Naik <kavyanaik2112@gmail.com>
2025-02-08 20:52:46 +0530

    added lazy loading

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-import About from "./components/About";
 import Contact from "./components/Contact";
 import RestaurantMenu from "./components/RestaurantMenu";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import Error from "./components/Error";
+
+const About = lazy(() => import("./components/About"));
 const AppLayout = () => {
   return (
     <div className="app">
@@ -26,15 +27,22 @@ const appRouter = createBrowserRouter([
         path: "/",
         element: <Body />,
       },
-      { path: "/about", element: <About /> },
+      {
+        path: "/about",
+        element: (
+          <Suspense fallback={<h1>Loading........</h1>}>
+            <About />
+          </Suspense>
+        ),
+      },
       {
         path: "/contact",
         element: <Contact />,
       },
       {
-        path: '/restaurant/:resId',
-        element: <RestaurantMenu/>
-      }
+        path: "/restaurant/:resId",
+        element: <RestaurantMenu />,
+      },
     ],
   },
 ]);
