956e43ac0054993b67a458bcb6572bbf660e5f44
Kavya Naik <kavyanaik2112@gmail.com>
2025-02-09 12:59:20 +0530

    added great discounts label - higher order components

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,4 +1,4 @@
-import RestaurantCard from "./RestaurantCard";
+import RestaurantCard, { withDiscountInfo } from "./RestaurantCard";
 import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
 import { Link } from "react-router-dom";
@@ -6,12 +6,15 @@ import useOnlineStatus from "../utils/useOnlineStatus";
 
 const Body = () => {
   const [lisOfRestaurants, setlisOfRestaurants] = useState([]);
-  const [resToSearch, setResToSearch] = useState('')
-  const [filteredListOfRestaurants, setfilteredListOfRestaurants] = useState([])
+  const [resToSearch, setResToSearch] = useState("");
+  const [filteredListOfRestaurants, setfilteredListOfRestaurants] = useState(
+    []
+  );
 
   const onlineStatus = useOnlineStatus();
-  console.log('online status', onlineStatus)
+  console.log("online status", onlineStatus);
 
+  const RestaurantCardWithDiscountInfo = withDiscountInfo(RestaurantCard);
   useEffect(() => {
     fetchData();
   }, []);
@@ -24,10 +27,15 @@ const Body = () => {
     setlisOfRestaurants(
       jsonData.data.cards[1].card.card.gridElements.infoWithStyle.restaurants
     );
-    setfilteredListOfRestaurants(jsonData.data.cards[1].card.card.gridElements.infoWithStyle.restaurants)
+    setfilteredListOfRestaurants(
+      jsonData.data.cards[1].card.card.gridElements.infoWithStyle.restaurants
+    );
+    console.log(
+      jsonData.data.cards[1].card.card.gridElements.infoWithStyle.restaurants
+    );
   };
-  if(!onlineStatus) {
-    return <h1> oops you are offline</h1>
+  if (!onlineStatus) {
+    return <h1> oops you are offline</h1>;
   }
   if (lisOfRestaurants.length === 0) {
     return <Shimmer></Shimmer>;
@@ -36,15 +44,27 @@ const Body = () => {
     <div className="p-5">
       <div className="flex justify-center flex-wrap">
         <div className="">
-          <input className="border-1 rounded-md" value={resToSearch} onChange={(e) => {
-            setResToSearch(e.target.value)
-          }}></input>
-          <button className="bg-gray-300 rounded-md p-2 ml-3" onClick={()=> {
-            const filteredOnes = lisOfRestaurants.filter((restaurant) => {
-              return restaurant.info.name.toLowerCase().includes(resToSearch.toLowerCase())
-            })
-            setfilteredListOfRestaurants(filteredOnes)
-          }}> Search</button>
+          <input
+            className="border-1 rounded-md"
+            value={resToSearch}
+            onChange={(e) => {
+              setResToSearch(e.target.value);
+            }}
+          ></input>
+          <button
+            className="bg-gray-300 rounded-md p-2 ml-3"
+            onClick={() => {
+              const filteredOnes = lisOfRestaurants.filter((restaurant) => {
+                return restaurant.info.name
+                  .toLowerCase()
+                  .includes(resToSearch.toLowerCase());
+              });
+              setfilteredListOfRestaurants(filteredOnes);
+            }}
+          >
+            {" "}
+            Search
+          </button>
         </div>
         <button
           className="bg-gray-300 rounded-md p-2 ml-20"
@@ -62,7 +82,17 @@ const Body = () => {
       </div>
       <div className="flex flex-wrap p-2 mt-10">
         {filteredListOfRestaurants.map((restaurant) => (
-          <Link key={restaurant.info.id} to={'/restaurant/'+restaurant.info.id}><RestaurantCard  resData={restaurant} /></Link>
+          <Link
+            key={restaurant.info.id}
+            to={"/restaurant/" + restaurant.info.id}
+          >
+            {/** if restaurant has aggregatedDiscountInfoV2 then display it, using higherOrder Component*/}
+            {restaurant.info.aggregatedDiscountInfoV2 ? (
+              <RestaurantCardWithDiscountInfo resData={restaurant} />
+            ) : (
+              <RestaurantCard resData={restaurant} />
+            )}
+          </Link>
         ))}
       </div>
     </div>
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -7,10 +7,7 @@ const RestaurantCard = (props) => {
     <div className="p-4 bg-gray-100 m-2 w-55 rounded-xl h-120 hover:bg-gray-200">
       <img
         className="h-50 w-50 rounded-lg"
-        src={
-          CDN_URL +
-          info.cloudinaryImageId
-        }
+        src={CDN_URL + info.cloudinaryImageId}
       ></img>
       <h3>{info.name}</h3>
       <h3>{info.avgRating} Stars</h3>
@@ -19,6 +16,17 @@ const RestaurantCard = (props) => {
       <h3>Delivery Time: {info.sla.slaString}</h3>
     </div>
   );
+};
+
+export const withDiscountInfo = (RestaurantCard) => {
+  return (props) => {
+    return (
+      <div className="relative inline-block">
+        <label className="absolute top-2 left-2 bg-black text-white text-xs font-semibold px-2 py-1 rounded ml-1 mt-1">Great Discounts</label>
+        <RestaurantCard {...props} />
+      </div>
+    );
+  };
 };
 
 export default RestaurantCard;
